ce19d4c8466c33cf4dd7d2d822435d7ca94db2ba
MahmoudIbrahim1012 <mahmoud20191700591@cis.asu.edu.eg>
2021-02-02 22:07:52 +0200

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(home: Home()));
+
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: Text(
+          "Movie App",
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
