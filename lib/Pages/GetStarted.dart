ff138c745b95adf841e09245e346f17c9f71a37e
MahmoudIbrahim1012 <mahmoud20191700591@cis.asu.edu.eg>
2021-02-16 01:39:26 +0200

    Get Started page scaffolding

--- lib/Pages/GetStarted.dart
+++ lib/Pages/GetStarted.dart
@@ -5,7 +5,32 @@ class GetStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network("https://i.pinimg.com/564x/8d/2d/1c/8d2d1c5e0ee9e5141f1fc51567dba572.jpg",
+                height: 50,
+                width: 50,
+                ),
+                SizedBox(width: 10),
+                Text("MOVIES TIME",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+                ),
+              ], // Row Children
+            ),
+            SizedBox(height: 250,),
+          ], // Column Children
+        ),
+      ),
       bottomNavigationBar: NavigationBar(currentIndex: 3),
     );
   }
